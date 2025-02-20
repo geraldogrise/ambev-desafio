2203b72d1a40a344a274724c0279d399c2dd05f1
geraldogrise <grise888@gmail.com>
2025-02-19 21:06:15 -0300

    desafio ambev
    desafio ambev

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Common/Result/PagedResult.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ambev.DeveloperEvaluation.Common.Result
+{
+
+    /// <summary>
+    /// Represents a paginated result set.
+    /// </summary>
+    /// <typeparam name="T">The type of items in the result set.</typeparam>
+    public class PagedResult<T>
+    {
+        /// <summary>
+        /// List of items in the current page.
+        /// </summary>
+        public IEnumerable<T> Data { get; set; } = new List<T>();
+
+        /// <summary>
+        /// Total number of items across all pages.
+        /// </summary>
+        public int TotalItems { get; set; }
+
+        /// <summary>
+        /// Current page number.
+        /// </summary>
+        public int CurrentPage { get; set; }
+
+        /// <summary>
+        /// Total number of pages available.
+        /// </summary>
+        public int TotalPages { get; set; }
+    }
+}
